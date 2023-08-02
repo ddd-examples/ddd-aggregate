22a993685021ff337a2be3f01e1b6baf61c8e864
parikshitTV <pnavgire998@gmail.com>
2023-08-02 21:36:51 +0530

    feat: implementing addition and removal of car items

---
+++ src/main/java/com/starkmotors/domain/Cart.java
@@ -0,0 +1,85 @@
+package com.starkmotors.domain;
+
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class Cart {
+    private CartId cartId;
+    private List<CartItem> items = new ArrayList<>();
+    private Money totalCost = new Money(0F, Currency.getInstance("INR"));
+
+    private Cart(CartId cartId) {
+        this.cartId = cartId;
+    }
+
+    public static Cart newCart() {
+        return new Cart(CartId.newCarId());
+    }
+
+    private void updateTotal() {
+        totalCost = items.stream()
+                .map(CartItem::getSubTotal)
+                .reduce(Money::add)
+                .orElse(Money.indianRupees(0F));
+    }
+
+    public CartId getCartId() {
+        return cartId;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public Money getTotalCost() {
+        return totalCost;
+    }
+
+    public void remove(AccessoryId accessoryId) {
+        Optional<CartItem> toBeRemoved = items.stream().filter(i -> i.getAccessory().getAccessoryId().equals(accessoryId)).findAny();
+        toBeRemoved.ifPresent(item -> {
+            items.remove(item);
+            updateTotal();
+        });
+    }
+
+    public void incrementQuantity(AccessoryId accessoryId) {
+        items.stream()
+                .filter(item -> item.getAccessory().getAccessoryId().equals(accessoryId))
+                .findAny()
+                .ifPresent(CartItem::incrementQuantity);
+        updateTotal();
+    }
+
+    public void addItem(Accessory accessory, int quantity) {
+        items.add(CartItem.forAccessory(accessory, quantity));
+        updateTotal();
+    }
+}
+
+class CartId {
+    private UUID uuid;
+
+    public CartId(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public static CartId newCarId() {
+        return new CartId(UUID.randomUUID());
+    }
+}
+
+class AccessoryId {
+    private UUID uuid;
+
+    public AccessoryId(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public static AccessoryId newAccessoryId() {
+        return new AccessoryId(UUID.randomUUID());
+    }
+}
